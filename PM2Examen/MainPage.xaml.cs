d228d8ffd33966bdf4df4cbb595e48258f2ffa16
estebancerrato <estebancerrato@hotmail.com>
2022-06-18 17:13:15 -0600

    se agrego lo de añadir imagen, falta la base de datos

--- PM2Examen/MainPage.xaml.cs
+++ PM2Examen/MainPage.xaml.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Essentials;
+using Plugin.Media;
+using System.IO;
 
 namespace PM2Examen
 {
@@ -13,6 +16,76 @@ namespace PM2Examen
         public MainPage()
         {
             InitializeComponent();
+            obtenerLatitudLongitud();
+        }
+
+        public async void obtenerLatitudLongitud()
+        {
+            try
+            {
+                var georequest = new GeolocationRequest(GeolocationAccuracy.Best, TimeSpan.FromSeconds(10));
+
+                var tokendecancelacion = new System.Threading.CancellationTokenSource();
+
+                var localizacion = await Geolocation.GetLocationAsync(georequest, tokendecancelacion.Token);
+
+
+                if (localizacion != null)
+                {
+                    txtlatitud.Text = localizacion.Latitude.ToString();
+                    txtlongitud.Text = localizacion.Longitude.ToString();
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                await DisplayAlert("Advertencia", "Este dispositivo no soporta GPS"+ fnsEx, "Ok");
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                await DisplayAlert("Advertencia", "Error de Dispositivo "+ fneEx, "Ok");
+            }
+            catch (PermissionException pEx)
+            {
+                await DisplayAlert("Advertencia", "Sin Permisos de Geolocalizacion" + pEx, "Ok");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Advertencia", "Sin Ubicacion " + ex, "Ok");
+            }
+        }
+
+        byte[] imageToSave;
+        private async void AddImg(object sender, EventArgs e)
+        {
+            try
+            {  
+
+                var takepic = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    Directory = "PhotoApp",
+                    Name = DateTime.Now.ToString() + "_foto.jpg",
+                    SaveToAlbum = true
+                });
+
+                await DisplayAlert("Ubicacion de la foto: ", takepic.Path, "Aceptar");
+
+                if (takepic != null)
+                {
+                    imageToSave = null;
+                    MemoryStream memoryStream = new MemoryStream();
+
+                    takepic.GetStream().CopyTo(memoryStream);
+                    imageToSave = memoryStream.ToArray();
+
+                    img.Source = ImageSource.FromStream(() => { return takepic.GetStream(); });
+                }
+                obtenerLatitudLongitud();
+                txtdescripcion.Focus();//le pasamos el puntero a la descripcion
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", "Se ha generado el siguiente error al agregar la imagen "+ex, "Aceptar");
+            }
         }
     }
 }
